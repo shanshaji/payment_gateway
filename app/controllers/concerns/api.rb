2c71dbf38ecfb7204561260b1fe41dbe7d5cfa22
SHAN SHAJI <shanshaji@gmail.com>
2018-08-06 17:11:11 +0530

    removed pry

--- app/controllers/concerns/api.rb
+++ app/controllers/concerns/api.rb
@@ -1,13 +1,11 @@
 module Api
 
 	def self.do_request(url, params={})
-	  binding.pry
 	  encrypted={}
 	  encrypted[:data]=params
 	  url = url + '?' + encrypted.to_query
 	  uri = URI.parse(url)
 	  response = Net::HTTP.get_response(uri)
 	  JSON.parse(response) if response
-	  binding.pry
 	end
 end
